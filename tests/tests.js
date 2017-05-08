6f729b232dd7202fe03d3c8581122326dee147ff
3m3ka <emeka.l.okoro@gmail.com>
2017-05-08 22:42:37 +0100

    add deep equal

--- tests/tests.js
+++ tests/tests.js
@@ -18,3 +18,24 @@ describe("should include or not include the following values", function() {
   });
 
 });
+
+describe("should deep equal the function call for the following values", function() {
+
+  it("should return an empty array for an input of 1", function() {
+    expect(getPrimes(1)).to.deep.equal([]);
+  });
+
+  it('should deep equal [2, 3, 5] for an input of 5', function () {
+      expect(getPrimes(5)).to.deep.equal([2, 3, 5]);
+  });
+
+  it("should deep equal [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47] for an input of 50", function() {
+    expect(getPrimes(50)).to.eql([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]);
+  });
+
+  it("should return an empty array for an input of []", function() {
+    expect(getPrimes([])).to.deep.equal([]);
+  });
+  
+
+});
